c4452b1d31a15ba83b521b959dfe438c561cd540
thiba <thiba@LAPTOP-GA0195NS>
2023-04-28 13:50:11 +0200

    a

--- bellefleur/Program.cs
+++ bellefleur/Program.cs
@@ -4,22 +4,67 @@ using Org.BouncyCastle.Security;
 using System.Configuration;
 using System.Runtime.ExceptionServices;
 using System.Xml;
+using bellefleur;
 
 
-Console.WriteLine("----Bienvenue dans la base de donnée Belle Fleur----\n Veuillezz appuyer sur une touche...");
+//----INITIALISATION----//
+Console.WriteLine("----Bienvenue dans la base de donnée Belle Fleur----\n Veuillez appuyer sur une touche...");
 Console.ReadKey();
 Console.Clear();
-Console.WriteLine("Veuillez taper votre identifiant :");
+
+//----CONNEXION AVEC LES IDENTIFIANTS----//
+Dictionary<string, string> valid_ids = User.Users;
+
+Console.Write("Veuillez taper votre identifiant : ");
 string username = Console.ReadLine();
+while (!valid_ids.ContainsKey(username))
+{
+    Console.Clear();
+    Console.Write("Veuillez saisir un identifiant valide : ");
+    username = Console.ReadLine();
+}
+Console.Clear();
+
+Console.Write("Veuillez taper votre mot de passe : ");
+string mdp = Console.ReadLine();
+while (!(valid_ids.TryGetValue(username, out string true_mdp)&& mdp==true_mdp) )
+{
+    Console.Clear();
+    Console.Write("Veuillez saisir un mot de passe valide : ");
+    mdp = Console.ReadLine();
+}
+Console.Clear();
+
+try
+{
+    string connectionString = "SERVER=localhost;PORT=3301;database=fleurs;UID="+username+";PASSWORD="+mdp+";";
+    MySqlConnection connection = new MySqlConnection(connectionString);
+    connection.Open();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 
-string connectionString = "SERVER=localhost;PORT=3301;database=loueur;UID=root;PASSWORD=root;";
-MySqlConnection connection = new MySqlConnection(connectionString);
-connection.Open();
 
 //PREMIER MENU POUR Root/root
 //Faire affichage et test sur la valeur de choice
-int choice1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Que voulez-vous faire ?\n"+
+                   "1-Lire la base de donnée\n" +
+                   "2-Ajouter un élément\n" +
+                   "3-Modifier un élément\n" +
+                   "4-Voir les statistiques\n" +
+                   "5-Exporter un fichier XML\n" +
+                   "6-Exporter un fichier JSON\n" +
+                   "7-Déconnexion" );
+Console.Write("Selectionner votre action(1 à 7) : ");
+int choice1 = Convert.ToInt32(Console.ReadLine());
+while ((choice1 is int)&&(choice1 < 1 || choice1 > 7))
+{
+    Console.WriteLine("Veuillez saisir un choix valide");
+    choice1 = Convert.ToInt32(Console.ReadLine());
+}
 switch (choice1)
 {
     case 1:Lecture_DB();
@@ -34,31 +79,133 @@ switch (choice1)
         break;
     case 6:Export_JSON();
         break;
+    case 7://Deconnexion
+        break;
     default:break;
 }
 
+//A FAIRE
 static void Lecture_DB()
 {
+    int choice1 = Convert.ToInt32(Console.ReadLine());
+    while (choice1 < 1 || choice1 > 7)
+    {
+        Console.WriteLine("Veuillez saisir un choix valide");
+        choice1 = Convert.ToInt32(Console.ReadLine());
+    }
+    switch (choice1)
+    {
+        case 1:
+            //Liste clients
+            break;
+        case 2:
+            //Liste commandes
+            break;
+        case 3:
+            //Stocks
+            break;
+        case 4:
+            //Quitter;
+            break;
+        default: break;
+    }
 
 }
 
+//A FAIRE
 static void Ajout_DB()
 {
-
+    int choice1 = Convert.ToInt32(Console.ReadLine());
+    while (choice1 < 1 || choice1 > 7)
+    {
+        Console.WriteLine("Veuillez saisir un choix valide");
+        choice1 = Convert.ToInt32(Console.ReadLine());
+    }
+    switch (choice1)
+    {
+        case 1:
+            //Ajout clients
+            break;
+        case 2:
+            //Ajout commandes
+            break;
+        case 3:
+            //Ajout bouquet type
+            break;
+        case 4:
+            //Quitter
+            break;
+        default: break;
+    }
 }
+
+//A FAIRE
 static void Modifier_DB()
 {
-
+    int choice1 = Convert.ToInt32(Console.ReadLine());
+    while (choice1 < 1 || choice1 > 7)
+    {
+        Console.WriteLine("Veuillez saisir un choix valide");
+        choice1 = Convert.ToInt32(Console.ReadLine());
+    }
+    switch (choice1)
+    {
+        case 1:
+            //Modifier clients
+            break;
+        case 2:
+            //Modifier commandes
+            break;
+        case 3:
+            //Modifier bouquet type
+            break;
+        case 4:
+            //Modifier stocks
+            break;
+        case 5:
+            //Quitter
+            break;
+        default: break;
+    }
 }
+
+//A FAIRE
 static void Show_Stats()
 {
-
+    int choice1 = Convert.ToInt32(Console.ReadLine());
+    while (choice1 < 1 || choice1 > 7)
+    {
+        Console.WriteLine("Veuillez saisir un choix valide");
+        choice1 = Convert.ToInt32(Console.ReadLine());
+    }
+    switch (choice1)
+    {
+        case 1:
+            //Ventes
+            break;
+        case 2:
+            //Produits les plus utilisés
+            break;
+        case 3:
+            //Gains = factures - prix composants
+            break;
+        case 4:
+            //Clients (plus de comandes etc...)
+            break;
+        case 5:
+            //Quitter
+            break;
+        default: break;
+    }
 }
 
+//A FAIRE
 static void Export_XML()
 {
 
 }
+
+//A FAIRE
 static void Export_JSON()
 {
 
